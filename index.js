2d0bb3af85c666e0c26f294d7bd37ee1c0f0feed
Bladimir Gerson Parra Bermudez <info@ipproyectosysoluciones.com>
2022-12-21 00:18:05 -0500

    Primera ruta '/'
    Signed-off-by: Bladimir Gerson Parra Bermudez <info@ipproyectosysoluciones.com>

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ const express = require( 'express' );
 //Crear Servidor express
 const app = express();
 
+//Rutas
+app.get( '/', ( req, res ) => {
+  
+  res.json(
+    {
+    ok: true,
+    msg: 'Hola Mundo',
+    }
+  );
+
+} );
+
 
 //Ejecutar el servidor
-app.listen( 3000, () =>
-{
+app.listen( 3000, () => {
   console.log( 'Servidor ejecutandose en el puerto ' + 3000 );
 } );
