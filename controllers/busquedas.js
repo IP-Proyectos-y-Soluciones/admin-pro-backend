79452ab1fa6ba737243cc57f2e45677004c5b19a
Bladimir Gerson Parra Bermudez <info@ipproyectosysoluciones.com>
2022-12-30 16:55:07 -0500

    Busqueda por colección específica
    Signed-off-by: Bladimir Gerson Parra Bermudez <info@ipproyectosysoluciones.com>

--- controllers/busquedas.js
+++ controllers/busquedas.js
@@ -1,18 +1,17 @@
-const { response } = require('express');
+const { response } = require("express");
 
-const Usuario = require( '../models/usuario' );
-const Medico = require( '../models/medico' );
-const Hospital = require( '../models/hospital' );
+const Usuario = require("../models/usuario");
+const Medico = require("../models/medico");
+const Hospital = require("../models/hospital");
 
 /**
  *
  * @param {*} req
  * @param {*} res
  */
-const getTodo = async ( req, res = response ) => {
-  
-  const search = req.params.search;
-  const regex = new RegExp( search, 'i' );
+const getTodo = async (req, res = response) => {
+	const search = req.params.search;
+	const regex = new RegExp(search, "i");
 
   const [ usuarios, medicos, hospitales ] = await Promise.all( [
     Usuario.find( { name: regex, } ),
@@ -20,14 +19,60 @@ const getTodo = async ( req, res = response ) => {
     Hospital.find( { name: regex, } ),
   ] );
 
-  res.json({
-    ok: true,
-    usuarios,
-    medicos,
-    hospitales,
-  });
+	res.json({
+		ok: true,
+		usuarios,
+		medicos,
+		hospitales,
+	});
 };
 
+/**
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ * @returns 
+ */
+const getDocCollection = async ( req, res = response ) => {
+  
+  const table = req.params.table;
+	const search = req.params.search;
+	const regex = new RegExp(search, "i");
 
+  let data = [];
 
-module.exports = { getTodo, };
+  switch ( table ) {
+    case 'medicos':
+      data = await Medico.find( { name: regex, } )
+                        .populate( 'usuario', 'name img' )
+                        .populate( 'hospital', 'name img' );
+      
+      break;
+    
+    case 'hospitales':
+      data = await Hospital.find( { name: regex, } )
+                          .populate( 'usuario', 'name img' );
+
+      break;
+
+    case 'usuarios':
+      data = await Usuario.find( { name: regex, } );
+
+      break;
+  
+    default:
+      return res.status( 400 ).json( {
+        ok: false,
+        msg: 'La table debe de ser /medicos /hospitales / usuarios',
+      } );
+
+  }
+    
+  res.json( {
+    ok: true,
+    resultados: data,
+  } );
+  
+};
+
+module.exports = { getTodo, getDocCollection, };
--- routes/busquedas.js
+++ routes/busquedas.js
@@ -1,17 +1,18 @@
 /**
  * Busquedas
- * Ruta: '/api/:search'
+ * Ruta: '/api/:search/'
  */
 
 const { Router } = require('express');
 const { validarJWT } = require( '../middlewares/validar-jwt' );
 
-const { getTodo } = require( '../controllers/busquedas' );
+const { getTodo, getDocCollection } = require( '../controllers/busquedas' );
 
 
 const router = Router();
 
-router.get( '/:search',validarJWT, getTodo );
+router.get( '/:search', validarJWT, getTodo );
+router.get( '/collection/:table/:search',validarJWT, getDocCollection );
 
 
 module.exports = router;
