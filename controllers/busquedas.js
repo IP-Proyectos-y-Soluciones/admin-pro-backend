b705463e3442131851164683866eaf4bdded689f
Bladimir Gerson Parra Bermudez <info@ipproyectosysoluciones.com>
2022-12-30 15:50:31 -0500

    Busqueda general en todas las colecciones
    Signed-off-by: Bladimir Gerson Parra Bermudez <info@ipproyectosysoluciones.com>

--- controllers/busquedas.js
+++ controllers/busquedas.js
@@ -1,5 +1,8 @@
 const { response } = require('express');
 
+const Usuario = require( '../models/usuario' );
+const Medico = require( '../models/medico' );
+const Hospital = require( '../models/hospital' );
 
 /**
  *
@@ -9,11 +12,19 @@ const { response } = require('express');
 const getTodo = async ( req, res = response ) => {
   
   const search = req.params.search;
+  const regex = new RegExp( search, 'i' );
+
+  const [ usuarios, medicos, hospitales ] = await Promise.all( [
+    Usuario.find( { name: regex, } ),
+    Medico.find( { name: regex, } ),
+    Hospital.find( { name: regex, } ),
+  ] );
 
   res.json({
     ok: true,
-    msg: 'getTodo',
-    search,
+    usuarios,
+    medicos,
+    hospitales,
   });
 };
 
